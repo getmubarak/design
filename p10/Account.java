9856776f3331bc94de669e0f00e4af9fae541f71
getmubarak <getmub@gmail.com>
2018-08-31 08:11:49 +0530

    Add files via upload

---
+++ p10/Account.java
@@ -0,0 +1,30 @@
+package problem10;
+
+public class Account {
+	double balance;
+	boolean email=false;
+	boolean sms=false;
+	boolean otp=false;
+	double limit=0;
+    //rsa
+	//secret question
+	
+	public void withDraw(double amount) {
+		
+		if(otp) {
+			//logic
+		}
+		if(limit > 0) {
+			//logic
+		}
+		
+		balance+=amount;
+		
+		if(email) {
+			//logic
+		}
+		if(sms) {
+			//logic
+		}
+	}
+}
