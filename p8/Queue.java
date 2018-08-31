913b6c155c2d701fa06acbe366a0c50e764c2a01
getmubarak <getmub@gmail.com>
2018-08-31 08:09:47 +0530

    Add files via upload

---
+++ p8/Queue.java
@@ -0,0 +1,11 @@
+package problem8;
+
+public class Queue {
+	//collection
+	public void enqueue(Object item) {
+		//logic to add item into collection
+	}
+	public void dequeue() {
+		//logic to remove item from collection
+	}
+}
