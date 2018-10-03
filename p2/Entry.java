2f26eea3f593fc1c0b737bcf67722c8474f75f49
getmubarak <getmub@gmail.com>
2018-10-03 13:57:26 +0530

    Update Entry.java

--- p2/Entry.java
+++ p2/Entry.java
@@ -3,12 +3,14 @@ package problem2;
 public class Entry {
 	public static void main()
 	{
-		Account a = new Account();
+		AccountService a = new AccountService();
 		a.withDraw(500);
 		a.deposit(7000);
 		a.withDraw(567);
 		a.deposit(6784);
 		a.withDraw(23);
 		a.withDraw(34);
+		a.undo();
+		a.undo();
 	}
 }
