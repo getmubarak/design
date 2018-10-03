b8099a22dc09db73413f0eca447f899f30611fcf
getmubarak <getmub@gmail.com>
2018-10-03 11:58:48 +0530

    Create Job.java

---
+++ p12/Job.java
@@ -0,0 +1,25 @@
+
+class Job
+{
+  void DoJob()
+  {
+    Domain domain = new Domain();
+    bool res = domain.fun();
+    if(res == true){
+      res = domain.fun2(100);
+      if(res == true){
+        Repostory rep = new Repository();
+        Emp emp = rep.get(1);
+        if(emp != null){	
+          ...
+        }else{
+          …
+        }
+      }else{
+        …
+      }			
+    }else{
+      …
+    }
+  }
+}
