140e88ef4cf5d5ab053dad77f25afd96ba5bb40d
getmubarak <getmub@gmail.com>
2018-10-03 13:55:09 +0530

    Create AccountService.java

---
+++ p2/AccountService.java
@@ -0,0 +1,27 @@
+class AccountService
+{
+  Stack<Operation> stack = new Stack<Operation>();
+  Account acc = new Account();
+  
+  public void withdraw(double amount){
+    acc.withdraw(amount);
+    Operation op = new Operation(1,amount);
+    stack.push(op);
+  }
+  
+  public void deposit(double amount){
+    acc.deposit(amount);
+    Operation op = new Operation(1,amount);
+    stack.push(op)
+  }
+  
+  public void undo(){
+    Operation op = stack.pop();
+    if(op.Type == 1)
+      acc.deposit(op.amount);
+    if(op.Type == 2)
+      acc.withdraw(op.amount);
+      
+  }
+  
+}
