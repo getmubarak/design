b3e64e711c4cfa32d22166b65a1e64eaf1adf66a
getmubarak <getmub@gmail.com>
2018-11-14 15:33:45 +0530

    Create Handler.java

---
+++ p13/Handler.java
@@ -0,0 +1,21 @@
+
+
+
+class Handler
+{
+     public void Collide(GameObject go1,GameObject go2)
+     {
+          if(type(o1)==type(Ship) && type(o2)==type(Station))
+          {
+            //logic1
+          }
+           if(type(o1)==type(Station) && type(o2)==type(Aestroid))
+          {
+            //logic2
+          }
+           if(type(o1)==type(Ship) && type(o2)==type(Commet))
+          {
+            //logic3
+          }
+     }
+}
