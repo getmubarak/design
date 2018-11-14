ca2c9479064ef763bedf222e2f53c53bb9b221ba
getmubarak <getmub@gmail.com>
2018-11-14 15:40:59 +0530

    Create GO.java

---
+++ p13/GO.java
@@ -0,0 +1,20 @@
+
+class GameObject
+{
+}
+
+class Ship extends GameObject
+{
+}
+
+class Station extends GameObject
+{
+}
+
+class Commet extends GameObject
+{
+}
+
+class Aestroid extends GameObject
+{
+}
