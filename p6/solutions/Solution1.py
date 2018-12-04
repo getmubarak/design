20c3650562645a36dd4d4cb93e74f0721dda2869
getmubarak <getmub@gmail.com>
2018-12-04 10:59:01 +0530

    Update Solution1.py

--- p6/solutions/Solution1.py
+++ p6/solutions/Solution1.py
@@ -1,45 +1,43 @@
 class Invoice:
   def __init__(self):
-   self.taxType = 0
+  self.taxType = 0
 
-  def getTaxType():
-    return taxType
+def getTaxType():
+  return taxType
 
-  def setTaxType(taxType):
-    self.taxType = taxType
+def setTaxType(taxType):
+  self.taxType = taxType
 
-  def getTotal():
-    amount = getSubtotal();
-    tax = Tax();
-    amount += tax.compute(amount, taxType);
-    return amount
+def getTotal():
+  amount = getSubtotal();
+tax = Tax();
+amount += tax.compute(amount, taxType);
+return amount
 
-  def getSubtotal():
-    subtotal = 0;
-    ‘’’
-    foreach(lineItem in Items) {
-    subtotal += lineItem.getPrice() * lineItem.Qty()
-    }
-    ‘’’
-    return subtotal;
+def getSubtotal():
+  subtotal = 0;‘’’
+foreach(lineItem in Items) {
+  subtotal += lineItem.getPrice() * lineItem.Qty()
+}‘’’
+return subtotal;
 
 class Tax:
   def __init__(self):
-   pass
+  pass
 
-  def compute(amount, taxType):
-    taxAmount = 0;
-    if taxType == 1:
-      taxAmount = amount * 0.05
-    if taxType == 2:
-      taxAmount = amount * 0.025 + 500
-    if taxType == 3:
-      taxAmount = (amount - 5000) * 0.3
-    return taxAmount
+def compute(amount, taxType):
+  taxAmount = 0;
+if taxType == 1:
+  taxAmount = amount * 0.05
+if taxType == 2:
+  taxAmount = amount * 0.025 + 500
+if taxType == 3:
+  taxAmount = (amount - 5000) * 0.3
+return taxAmount
 
 def main():
   inv = Invoice()
-  inv.setTaxType(3)# add item
-  amount = inv.getTotal()
+inv.setTaxType(3)# add item
+amount = inv.getTotal()
 
 main()
