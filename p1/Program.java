cb7bd79642aa7a6160cf5755e06e0e8afd03de96
getmubarak <getmub@gmail.com>
2018-12-17 12:01:48 +0530

    Create Program.java

---
+++ p1/Program.java
@@ -0,0 +1,21 @@
+class Program
+{
+  public static void Main()
+  {
+    StopWatch w = new StopWatch();
+    w.Start();
+    ..
+    ..
+    ..
+    w.Start();
+    ..
+    ..
+    w.Stop();
+    ..
+    ..
+    w.Stop();
+  
+  
+  
+  }
+}
