a507deb67b143a7f1922ee40b9aa31504907435a
getmubarak <getmub@gmail.com>
2018-12-18 12:22:32 +0530

    Update Entry2.java

--- p5/Entry2.java
+++ p5/Entry2.java
@@ -5,14 +5,18 @@ import java.util.HashMap;
 public class Entry {
 	public static void main(String[] args) 
     {
-		    RuleEngine engine = new RuleEngine();
+        RuleEngine engine = new RuleEngine();
 	       
-	      //String expression = "(salary > 2500 or age < 30) or (department = 'purchase')";
-		    Rule r1 = new Rule("salary","2500",1);
+	//String expression = "(salary > 2500 or age < 30) or (department = 'purchase')";
+	Rule r1 = new Rule("salary","2500",1);
         Rule r2 = new Rule("age","30",2);
         Rule r3 = new Rule("department","purchase",3);
         
-        engine.AddRule("rule1",expression);
+	RuleSet rs = new RuleSet();
+	rs.add(r1);
+	rs.add(r2);
+	rs.add(r3);		
+        engine.AddRule("rule1",rs);
 		
      
         HashMap<String, Object> object = new HashMap<>(); 
