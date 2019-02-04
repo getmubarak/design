3ceeca69cc2c5112189274bd5c0dd5190340f03b
getmubarak <getmub@gmail.com>
2019-02-04 13:58:27 +0530

    Create Tax.java

---
+++ p19/Tax.java
@@ -0,0 +1,32 @@
+class Tax
+{ 
+  private int taxType;
+	
+	
+	public int getTaxType() {
+		return taxType;
+	}
+
+	public void setTaxType(int taxType) {
+		this.taxType = taxType;
+	}
+	
+  public double compute(int taxType, double amount){
+      double taxAmount =0;
+  
+      switch(taxType)
+      {
+        case 1:
+          taxAmount = amount * 0.05;
+          break;
+        case 2:
+          taxAmount= amount * 0.025 + 500;
+          break;
+        case 3:
+          taxAmount= (amount- 5000) * 0.3;
+          break;
+      }
+      return taxAmount;
+  }
+  
+}
