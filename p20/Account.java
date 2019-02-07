2fe2b3c12cde6027c73de81e7d2ec63e5f59f602
getmubarak <getmub@gmail.com>
2019-02-07 14:01:26 +0530

    Create Account.java

---
+++ p20/Account.java
@@ -0,0 +1,31 @@
+
+
+interface Account
+{
+     public withdraw(double amount);
+     public deposit(double amount);
+}
+
+class SA implements Account
+{
+     public withdraw(double amount)
+     {
+        //logic
+     }
+     public deposit(double amount)
+     {
+        //logic
+     }
+}
+
+class CA implements Account
+{
+     public withdraw(double amount)
+     {
+        //logic
+     }
+     public deposit(double amount)
+     {
+        //logic
+     }
+}
