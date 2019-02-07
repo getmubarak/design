cffa3a722b30f54661b74f06bc1aa01245060d84
getmubarak <getmub@gmail.com>
2019-02-07 16:11:00 +0530

    Update Entry.java

--- p9/Entry.java
+++ p9/Entry.java
@@ -14,19 +14,7 @@ public class Entry {
 			dlg.Display();			
 		}
 	}
-	public static void Save(Account account) {
-		Dao dao=null;
-		
-		if(account instanceof SA) {
-			dao = new SADao();
-		}
-		if(account instanceof CA) {
-			dao = new CADao();
-		}
-		if(dao!=null) {
-			dao.save(account);			
-		}
-	}
+	
 	static void main()
 	{
 		Account account = new SA();
