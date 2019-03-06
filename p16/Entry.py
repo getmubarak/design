576a056cf958a2bf60643150a0592152b6e9b8ef
getmubarak <getmub@gmail.com>
2019-03-06 11:29:37 +0530

    Update Entry.py

--- p16/Entry.py
+++ p16/Entry.py
@@ -1,1 +1,28 @@
 
+enum State
+{
+  opened,
+  assigned,
+  resolved,
+  rejected,
+  closed
+}
+class Bug
+{
+  int budId;
+  string desc;
+  State curState;
+}
+
+
+class Entry
+{
+  public void changeState(Bug bug,State destState)
+  {
+    if(destState == State.opened && (bug.curState != State.resolved  || bug.curState != State.rejected))
+        throw new InvalidStateException();
+    //...
+    
+    bug.curState = destState;
+  }
+}
