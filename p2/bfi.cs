91af368ec1dd76ccdeb9c6ece9b5c045afb099f8
getmubarak <getmub@gmail.com>
2019-05-09 10:59:12 +0700

    Update bfi.cs

--- p2/bfi.cs
+++ p2/bfi.cs
@@ -22,3 +22,36 @@ class DepositOp: Operation
       acc.Withdraw(amount);
   }
 }
+
+Operation 
+WithdrawOp : Operation 
+DepositOp : Operation
+
+class AccountService
+{
+  Stack<Operation> stack = new Stack<Operation>();
+  
+  Account acc = new Account();
+  
+  public void withdraw(double amount){
+    acc.withdraw(amount);
+    WithdrawOpn op = new WithdrawOpn;
+    op.amount = amount;
+    op.acc = acc;
+    stack.push(op);
+  }
+  
+  public void deposit(double amount){
+    acc.deposit(amount);
+     DepositOpn op = new DepositOpn;
+    op.amount = amount;
+    op.acc = acc;
+    stack.push(op)
+  }
+  
+  public void undo(){
+    Operation op = stack.pop();
+    op.undo();  
+  }
+  
+}
