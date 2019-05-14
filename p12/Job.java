2a12a9665d3ecc2158e0e20b9d16c5cb0cd2e0ae
getmubarak <getmub@gmail.com>
2019-05-14 16:04:37 +0530

    Update Job.java

--- p12/Job.java
+++ p12/Job.java
@@ -1,25 +1,21 @@
 
 class Job
 {
-  void DoJob()
+  void do(int jobID)
   {
-    Domain domain = new Domain();
-    bool res = domain.fun();
-    if(res == true){
-      res = domain.fun2(100);
-      if(res == true){
-        Repostory rep = new Repository();
-        Emp emp = rep.get(1);
-        if(emp != null){	
-          ...
-        }else{
-          …
-        }
-      }else{
-        …
-      }			
-    }else{
-      …
-    }
+       switch(jobID)
+       {
+         case 1:
+           break;
+         case 2:
+           break;
+         case 3:
+           break;
+         case 4:
+           break;
+          ....
+          case n:
+           break;
+       }
   }
 }
