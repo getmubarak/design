3124b1b8be1729913d9e366a7a5e4d9f6cc92359
getmubarak <getmub@gmail.com>
2019-11-14 15:37:19 +0530

    Create AccountService.py

---
+++ p2/AccountService.py
@@ -0,0 +1,22 @@
+class AccountService:
+  def __init__(self):
+    self.stack = Stack()
+    self.acc = Account()
+  
+  def withdraw(self, amount):
+    acc.withdraw(amount)
+    op = Operation(1,amount)
+    stack.push(op)
+ 
+  def deposit(amount):
+    acc.deposit(amount)
+    op = new Operation(2,amount)
+    stack.push(op)
+  
+  
+  def undo():
+    op = stack.pop();
+    if(op.Type == 1):
+      acc.deposit(op.amount)
+    if(op.Type == 2):
+      acc.withdraw(op.amount)
