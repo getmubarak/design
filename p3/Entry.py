211bb491587e7c154d09baa61a6be69110dbcd62
getmubarak <getmub@gmail.com>
2019-11-15 13:58:58 +0530

    Update Entry.py

--- p3/Entry.py
+++ p3/Entry.py
@@ -1,13 +1,13 @@
-    connection =  SqlConnection()
-		connection.open("mydb;scott;tiger")
-		transaction =  SqlTransaction()
-		transaction.begin(connection)
+connection =  SqlConnection()
+connection.open("mydb;scott;tiger")
+transaction =  SqlTransaction()
+transaction.begin(connection)
 		
-	  cmd1 =  SqlCommand()
-		cmd1.execute(transaction, "insert into emp values(10,'jack',2500')")
+cmd1 =  SqlCommand()
+cmd1.execute(transaction, "insert into emp values(10,'jack',2500')")
 
-		cmd2 =  SqlCommand()
-		cmd2.execute(transaction, "insert into emp values(20,'jill',4300')")
+cmd2 =  SqlCommand()
+cmd2.execute(transaction, "insert into emp values(20,'jill',4300')")
 		
-		transaction.commit()
-		connection.close()
+transaction.commit()
+connection.close()
