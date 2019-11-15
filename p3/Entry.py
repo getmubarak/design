3ce40bb547db406ae73e988b3f39225e0d9ebf2a
getmubarak <getmub@gmail.com>
2019-11-15 13:58:17 +0530

    Create Entry.py

---
+++ p3/Entry.py
@@ -0,0 +1,13 @@
+    connection =  SqlConnection()
+		connection.open("mydb;scott;tiger")
+		transaction =  SqlTransaction()
+		transaction.begin(connection)
+		
+	  cmd1 =  SqlCommand()
+		cmd1.execute(transaction, "insert into emp values(10,'jack',2500')")
+
+		cmd2 =  SqlCommand()
+		cmd2.execute(transaction, "insert into emp values(20,'jill',4300')")
+		
+		transaction.commit()
+		connection.close()
