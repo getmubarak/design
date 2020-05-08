9bba8f3414e7b6c588fdfc1ee1f332f97f9435bc
getmubarak <getmub@gmail.com>
2020-05-08 14:09:31 +0530

    Update Entry.java

--- p5/Entry.java
+++ p5/Entry.java
@@ -3,27 +3,22 @@ package problem5;
 import java.util.HashMap;
 
 public class Entry {
-	public static void main(String[] args) 
+    public static void main(String[] args) 
     {
-		RuleEngine engine = new RuleEngine();
-	        
-	    String expression = "(salary > 2500 or age < 30) or (department = 'purchase')";
-		engine.AddRule("rule1",expression);
-		
-     
-        HashMap<String, Object> object = new HashMap<>(); 
-        object.put("Salary", 2500);
-        object.put("Age", 25);
-        object.put("Department", "Purchase");
-        boolean result = engine.Eval("rule1", object);        
-        System.out.print(result);   
-        
-        object = new HashMap<>(); 
+	Rule r1 = new Rule("Salary","5000",">");
+        Rule r2 = new Rule("Age","50","<");
+        Rule r3 = new Rule("Department","Purchase","=");
+        RuleSet rs = new RuleSet();
+	rs.add(r1);
+	rs.add(r2);
+	rs.add(r3);
+	
+	object = new HashMap<>(); 
         object.put("Salary", 3500);
         object.put("Age", 45);
         object.put("Department", "Sales");
-        result = engine.Eval("rule1", object);        
-        System.out.print(result);   
         
+        result = Eval(rs,object)
+        System.out.print(result);   
     }
 }
