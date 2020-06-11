4c5c6e1d84bbeda412a2f07a8681f504607c83e7
getmubarak <getmub@gmail.com>
2020-06-11 15:27:36 +0530

    Create Entry6.java

---
+++ p13/Entry6.java
@@ -0,0 +1,17 @@
+class CA {
+}
+class CB extends CA{
+}
+class CC extends CA{
+}
+//----------------------
+class CX {
+}
+class CY extends CX{
+}
+class CZ extends CX{
+}
+//-----------------------
+CA o1 = new ?;
+CX o2 = new ?;
+
