0b9be608487c5e38f5c3baef4e0ee5c919d1ae0a
getmubarak <getmub@gmail.com>
2020-06-12 11:11:12 +0530

    Update Job.java

--- p12/Job.java
+++ p12/Job.java
@@ -1,20 +1,25 @@
 
-class Job
+class JobServer
 {
-  void do(int jobID)
+  public void execute(int jobID,object data)
   {
        switch(jobID)
        {
          case 1:
+           //logic 1
            break;
          case 2:
+            //logic 2
            break;
          case 3:
+            //logic 3
            break;
          case 4:
+            //logic 4
            break;
           ....
           case n:
+            //logic n
            break;
        }
   }
