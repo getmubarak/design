075714a7f40ede437d6e2c65b2629f1bb2e2fad4
getmubarak <getmub@gmail.com>
2020-07-09 15:38:19 +0530

    Update Rule.java

--- p5/Rule.java
+++ p5/Rule.java
@@ -1,13 +1,29 @@
+//r1 = new Rule("age", "20",">")
+//r1.eval(obj)
+
 class Rule
 {
   String property;
   String value;
-  int operator;
+  char operator;
   
-  public Rule(String property,String value,int operator)
+  public Rule(String property,String value,char operator)
   {
     this.property = property;
     this.value = value;
     this.operator = operator;
+  }
+  bool eval(Map<string,object> obj)
+  {
+    if(!obj.contains(property))
+      return false;
+    
+    curValue = obj.get(property);
+    switch(operator)
+    {
+      case '>':
+        ...
+        break;
+    }
   }
 }
