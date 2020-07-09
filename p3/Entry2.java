841d59ad573b9c702aaba18eb853c0c955860b14
getmubarak <getmub@gmail.com>
2020-07-09 14:39:39 +0530

    Create Entry2.java

---
+++ p3/Entry2.java
@@ -0,0 +1,31 @@
+package problem3;
+
+interface Connection {
+	void open(String connectionString);
+	void close();
+}
+public class SqlConnection implements Connection {
+	public void open(String connectionString){
+		//logic
+	}
+	public void close(){
+		//logic
+	}
+}
+public class OraConnection  implements Connection {
+	public void open(String connectionString){
+		//logic
+	}
+	public void close(){
+		//logic
+	}
+}
+//3rd party
+public class MongoDB{
+	public void connect(string str){
+		//logic
+	}
+	public void disconnect(){
+		//logic
+	}
+}
