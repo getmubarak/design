81cdeeff614c338ba8ca4365f808f6eaea395523
getmubarak <getmub@gmail.com>
2020-07-09 14:56:54 +0530

    Create Entry.java

---
+++ p8/Entry.java
@@ -0,0 +1,29 @@
+public class List {
+	//collection
+	public void add(Object item,int index ) {
+		//logic to add item into collection
+	}
+	public void remove(int index ) {
+		//logic to remove item from collection
+	}
+}
+
+public class Queue {
+	//collection
+	public void enqueue(Object item) {
+		//logic to add item into collection
+	}
+	public void dequeue() {
+		//logic to remove item from collection
+	}
+}
+
+public class Stack {
+	//collection
+	public void push(Object item,int index ) {
+		//logic to add item into collection
+	}
+	public void pop(int index ) {
+		//logic to remove item from collection
+	}
+}
