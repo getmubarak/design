b742584a451cf01f74add90cb038f8040376959e
getmubarak <getmub@gmail.com>
2020-07-09 14:00:05 +0530

    Update Entry.java

--- p3/Entry.java
+++ p3/Entry.java
@@ -14,6 +14,14 @@ public class Entry {
 		cmd2.execute(transaction, "insert into emp values(20,'jill',4300')");
 		
 		transaction.commit();
+		
+		SqlTransaction transaction2 = new SqlTransaction();
+		transaction2.begin(connection);
+		
+		SqlCommand cmd3 = new SqlCommand();
+		cmd3.execute(transaction, "insert into emp values(10,'jack',2500')");
+
+		transaction2.commit();
 		connection.close();
 		
 	}
