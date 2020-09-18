33e216577f25e7f612c50335ee64c59dd398773d
getmubarak <getmub@gmail.com>
2020-09-18 15:16:24 +0530

    Create Entry2.java

---
+++ p8/Entry2.java
@@ -0,0 +1,19 @@
+
+class CA{
+  void fun(int x){
+    if(x < 500)
+      throw
+      
+    //logic
+  }
+}
+class CB extends CA{
+  void fun(int x){
+    if(x < 800)
+      throw
+      
+    //logic2
+  }
+}
+
+
