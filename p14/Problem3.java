f848f36ae7d8aefb773cdbdd70e7285c5d27d8e2
getmubarak <getmub@gmail.com>
2020-09-18 13:06:06 +0530

    Create Problem3.java

---
+++ p14/Problem3.java
@@ -0,0 +1,39 @@
+
+class CA{
+  void f1(){} //1
+  void f2(){} //2
+  void call(CX x){
+    x.f3(this);
+  }
+}
+class CB extends CA{
+  void f1(){} //3
+  void f2(){} //4
+  void call(CX x){
+    x.f3(this);
+  }
+}
+class CC extends CA{
+  void f1(){} //5
+  void f2(){} //6
+  void call(CX x){
+    x.f3(this);
+  }
+}
+//----------------------------------
+class CX
+{
+  void f3(CA a) {}
+  void f3(CB b) {}
+  void f3(CC c) {}
+}
+
+//----------------------------------
+void do(CA obj){
+  obj.f1(); //<- 1 | 3 | 5
+  obj.f2(); //<- 2 | 4 | 6
+  
+  CX x = new CX();
+  //x.f3(obj); ?
+  obj.call(x);
+}
