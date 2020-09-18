b261d45c68b14d1ea20062bde327161f26af7196
getmubarak <getmub@gmail.com>
2020-09-18 13:04:44 +0530

    Create Problem2.java

---
+++ p14/Problem2.java
@@ -0,0 +1,28 @@
+class CA{
+  void f1(){} //1
+  void f2(){} //2
+}
+class CB extends CA{
+  void f1(){} //3
+  void f2(){} //4
+}
+class CC extends CA{
+  void f1(){} //5
+  void f2(){} //6
+}
+//----------------------------------
+class CX
+{
+  void f3(CA a) {}
+  void f3(CB b) {}
+  void f3(CC c) {}
+}
+
+//----------------------------------
+void do(CA obj){
+  obj.f1(); //<- 1 | 3 | 5
+  obj.f2(); //<- 2 | 4 | 6
+  
+  CX x = new CX();
+  x.f3(obj); ?
+}
