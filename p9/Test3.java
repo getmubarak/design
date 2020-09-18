5ca40382c7c4d562e6b7f2f8d35140458d4f6ebc
getmubarak <getmub@gmail.com>
2020-09-18 11:31:21 +0530

    Update Test3.java

--- p9/Test3.java
+++ p9/Test3.java
@@ -1,17 +1,8 @@
 class CA{
-  void invoke(CX x){ //step 2
-    x.f(this);
-  }
 }
 class CB extends CA {
-  void invoke(CX x){//step 2
-    x.f(this);
- }
 }
 class CC extends CA {
-  void invoke(CX x){//step 2
-    x.f(this);
- }
 }
 class CX{
   void f(CA a) {} //1
@@ -28,16 +19,6 @@ class CZ extneds CX{
   void f(CB b) {} //8
   void f(CC c) {} //9 
 }
-
 void do(CX x, CA a){
-  a.invoke(x);//step 1 
-  
-  //x.f(a); //<-- 1 | 4 | 7
-   
-  if(a instanceof(CB))
-     x.f((CB)a);
-   else if(a instanceof(CC))
-     x.f((CC)a);
-  else
-    x.f(a)
+  x.f(a); 
 }
