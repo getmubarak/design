f1bf5a0bcec43f7deb5a63c80e6c6877ce5d7842
getmubarak <getmub@gmail.com>
2020-09-18 15:51:41 +0530

    Update Entry2.java

--- p7/Entry2.java
+++ p7/Entry2.java
@@ -4,47 +4,44 @@ interface AccountType{
 }
 class Platinum implements AccountType{
    void IsWithdrawInLimit(amount){
-      if(amount > 20000){
+      if(amount > 20000)
             throw; 
-       }
     }
 }
 class Silver implements AccountType{
     void IsWithdrawInLimit(amount){
-      if(amount > 10000){
+      if(amount > 10000)
             throw; 
-       }
     }
 }
 //====================================================
-
-class Account{
+interface Account{
+    void IsSufficientBalance(amount);
+    void withDraw(double amount);
+    void setType(AccountType type);
+}
+class AccountBase implments Account{
     double balance;
     AccountType type;
     
     void setType(AccountType type) {
 		  this.type = type;
-	  }
-    
-    abstract void IsSufficientBalance(amount);
-    
+    }
     public void withDraw(double amount) {		
-	        type. IsWithdrawInLimit(amount);
+	  type. IsWithdrawInLimit(amount);
           IsSufficientBalance(amount);
           ...
     }
 }
-class SA implements Account{
+class SA extends AccountBase{
     void IsSufficientBalance(amount){
-       if((balance - amount) < 5000){
-					throw;
-			  }
+       if((balance - amount) < 5000)
+		throw;
     }
 }
-class CA implements Account{
+class CA extends AccountBase{
     void IsSufficientBalance(amount){
-       if((balance - amount) < 100000){
-					throw;
-			  }
+       if((balance - amount) < 100000)
+		throw;
     }
 }
