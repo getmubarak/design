bdf4aa00a5e8533c77cc944dfe6d0a0ed626d6bb
getmubarak <getmub@gmail.com>
2020-09-18 13:13:13 +0530

    Create Problem5.java

---
+++ p14/Problem5.java
@@ -0,0 +1,50 @@
+interface Plugin{
+  void do(CA a);
+  void do(CB b);
+  void do(CC c);
+}
+class CA{
+  void f1(){} //1
+  void f2(){} //2
+  void call(Plugin p){
+    p.do(this);
+  }
+}
+class CB extends CA{
+  void f1(){} //3
+  void f2(){} //4
+  void call(Plugin p){
+    p.do(this);
+  }
+}
+class CC extends CA{
+  void f1(){} //5
+  void f2(){} //6
+  void call(Plugin p){
+    p.do(this);
+  }
+}
+//----------------------------------
+class F3 implements Plugin
+{
+  void do(CA a) {} //7
+  void do(CB b) {} //8
+  void do(CC c) {} //9
+}
+class F4 implements Plugin
+{
+  void do(CA a) {} //10
+  void do(CB b) {} //11
+  void do(CC c) {} //12
+}
+//----------------------------------
+void do(CA obj){
+  obj.f1(); //<- 1 | 3 | 5
+  obj.f2(); //<- 2 | 4 | 6
+  
+  F3 f3 = new F3();
+  f3.call(x); //<- 7| 8 | 9
+  
+  F4 f4 = new F4();
+  f4.call(x); //<- 10| 11 | 12
+}
