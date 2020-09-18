82e2ecdb178a04449d930e6c80e8014b97981258
getmubarak <getmub@gmail.com>
2020-09-18 16:22:44 +0530

    Create DataStructure.java

---
+++ summary/DataStructure.java
@@ -0,0 +1,20 @@
+Linked List
+
+class Emp{
+   Emp ref;
+}
+
+e1->e2->e3->e4->
+
+------------------------
+Tree
+
+class Emp{
+  List<Emp> emps;
+}
+
+e1->e2
+  ->e3->e4
+      ->e5
+
+------------------------
