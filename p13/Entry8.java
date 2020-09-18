9a53fc3daffc8d94f492c7b9990a445573c6aa7a
getmubarak <getmub@gmail.com>
2020-09-18 15:05:52 +0530

    Update Entry8.java

--- p13/Entry8.java
+++ p13/Entry8.java
@@ -1,6 +1,11 @@
 interface Action{
   void Collide(GameObject go1,GameObject go2);
 }
+class ShipCommet implements Action{
+  void Collide(GameObject go1,GameObject go2){
+    ... logic
+  }
+}
 class ShipStation implements Action{
   void Collide(GameObject go1,GameObject go2){
     ... logic
@@ -17,11 +22,12 @@ class Handler
      map<key,Action> vtable =new map<key,Action>();
      void Init() { 
       vtable.add("ShipStation", new  ShipStation());
+      vtable.add("ShipCommet", new  ShipCommet());
       ... 
      }
      public void Collide(GameObject go1,GameObject go2)
      {
-         Action action = vtable.get(go1.getKey() + go2.getKey());
+         Action action = vtable.get(go1.getclassName() + go2.getclassName());
          action.Collide(go1,go2);
      }
 }
