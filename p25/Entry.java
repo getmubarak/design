211912fb8e2c79f478ae450f07b0d7c3db929ce2
getmubarak <getmub@gmail.com>
2020-10-21 14:29:07 +0530

    Update Entry.java

--- p25/Entry.java
+++ p25/Entry.java
@@ -1,22 +1,25 @@
 class SurveyData:
-    def __init__(self):
-      self.path="" #save the data to this file.
-      self.hidden=False #should the file be hidden?
+    public SurveyData(){
+      self.path="";
+      self.hidden=False;
+    }
     
-    def setSavePath(self,t):
+    public void setSavePath(t){
         if t == 0:
             #raw data.
-            path = "c:/application/data/raw.dat"
+            path = "c:/application/data/raw.dat";
             hidden = True;
         elif t == 1:
             #cleaned up data.
-            path = "c:/application/data/cleanedUp.dat"
-            hidden = True
+            path = "c:/application/data/cleanedUp.dat";
+            hidden = True;
         elif t == 2:
             #processed data.
-            path = "c:/application/data/processed.dat"
-            hidden = True
+            path = "c:/application/data/processed.dat";
+            hidden = True;
         elif t == 3:
             #data ready for publication.
-            path = "c:/application/data/publication.dat"
-            hidden = False
+            path = "c:/application/data/publication.dat";
+            hidden = False;
+      }
+}
