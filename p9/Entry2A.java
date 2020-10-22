7a810402899dd5378b48efa22706162bf22c0d39
getmubarak <getmub@gmail.com>
2020-10-22 15:30:40 +0530

    Update Entry2A.java

--- p9/Entry2A.java
+++ p9/Entry2A.java
@@ -1,11 +1,11 @@
  public static void ShowUI(Account account) {
 		Dialog dlg=null;
 		Factory factory = new Factory();
-    if(account instanceof SA) {
-			dlg = factory.get((SADialog)account);
+                if(account instanceof SA) {
+			dlg = factory.get((SA)account);
 		}
 		if(account instanceof CA) {
-      dlg = factory.get((CADialog)account);
+                        dlg = factory.get((CA)account);
 		}
 		if(dlg!=null) {
 			dlg.Display();			
