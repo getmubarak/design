ba8f9cc4b9f27c83532050340d4aa904dbd72ede
getmubarak <getmub@gmail.com>
2020-10-22 16:00:42 +0530

    Create example3.java

---
+++ p9/example3.java
@@ -0,0 +1,23 @@
+class CA{}
+class CB exends CA{}
+class CC exends CA{}
+
+class CX{
+  void f(CA) {}//1
+  void f(CB) {}//2
+  void f(CC) {}//3
+}
+class CY. extends CX {
+  void f(CA) {}//4
+  void f(CB) {}//5
+  void f(CC) {}//6
+}
+class CZ. extends CX {
+  void f(CA) {}//7
+  void f(CB) {}//8
+  void f(CC) {}//9
+}
+
+do(CA a, CX x){
+  x.f(a); 
+}
