2b1ae8cd9695a5381f00ab4e05d77ea1136ef060
getmubarak <getmub@gmail.com>
2020-10-23 14:18:45 +0530

    Update Entry9.java

--- p13/Entry9.java
+++ p13/Entry9.java
@@ -2,13 +2,13 @@ class Handler
 {
      map<key,Lamda> vtable =new map<key,Lamda>();
      void Init() { 
-      vtable.add("ShipStation",()-> logic );
-      vtable.add("ShipShip",()-> logic );
+      vtable.add("ShipStation",()-> invoke logic );
+      vtable.add("ShipShip",()-> invoke logic );
       ...
      }
      public void Collide(GameObject go1,GameObject go2)
      {
-         Action action = vtable.get(go1.getKey() + go2.getKey());
-         action.Collide(go1,go2);
+         Lamda fun = vtable.get(go1.getKey() + go2.getKey());
+         fun(go1,go2);
      }
 }
