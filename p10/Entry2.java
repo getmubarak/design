09af6b0d183bc73803cce2a9dd58e7b7c44291bb
getmubarak <getmub@gmail.com>
2021-01-22 15:54:15 +0530

    Update Entry2.java

--- p10/Entry2.java
+++ p10/Entry2.java
@@ -1,48 +1,35 @@
+package problem10;
+
 interface Account{
-    void withDraw(double amount);
+	void withDraw(double amount);
 }
-public class AccountImp implements Account{
+class AccountImp implements Account {
 	double balance;
-  
+	double limit=0;
+	
 	public void withDraw(double amount) {
-		balance-=amount;
+		balance-=amount;<-- 5
+	}
+}
+class AccountEmailNotify implements Account {
+	Account ref;
+	
+	public void withDraw(double amount) {
+		ref.withDraw(amount);<-- 4
+		//email <-- 6
+	}
+}
+class AccountOtpAuth implements Account {
+	Account ref;
+	
+	public void withDraw(double amount) {
+		//otp<-- 2
+		ref.withDraw(amount);<-- 3
 	}
 }
 
 void Main(){
-  Account acc = new Otp(
-                new SecretQuestion(
-                new EmailNotification(
-                new SmsNotification(new AccountImp()))));
-  acc.withdraw(345);
-}
-
-class Otp implements Account{
-  Account ref;
-  public void withDraw(double amount) {
-		//... otp logic
-    ref.withDraw(amount);
-	}
-}
-class SecretQuestion implements Account{
-  Account ref;
-  public void withDraw(double amount) {
-		//... SecretQuestion logic
-    ref.withDraw(amount);
-	}
-}
-class EmailNotification implements Account{
-  Account ref;
-  public void withDraw(double amount) {
-    ref.withDraw(amount);
-    //... EmailNotification logic
-	}
-}
-class SmsNotification implements Account{
-  Account ref;
-  public void withDraw(double amount) {
-    ref.withDraw(amount);
-    //... SmsNotification logic
-	}
+	Account acc = new AccountOtpAuth(new AccountEmailNotify(new AccountSmsNotify(new AccountImp())));
+	acc.withdraw(100); <-- 1
 }
 
