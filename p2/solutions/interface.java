74b77ea3f70a5bbd1aeed8a26a6288dbed4819e6
getmubarak <getmub@gmail.com>
2021-02-10 16:40:19 +0530

    Create interface.java

---
+++ p2/solutions/interface.java
@@ -0,0 +1,35 @@
+interface Operation{
+  undo(Account acc);
+}
+class DepositOp implements Operation
+   double amount; 
+   public void undo(Account acc){
+     acc.withdraw(amount);
+   }
+}
+class WithdrawOp implements Operation
+   double amount; 
+   public void undo(Account acc){
+     acc.deposit(amount);
+   }
+}
+class AccountService 
+{
+  Stack<Operation> stack = new Stack<Operation>();
+  Account acc = new Account(); 
+  
+  public void withdraw(double amount){
+    acc.withdraw(amount);
+    Operation op = new WinthDrawOpr(amount);
+    stack.push(op);
+  }
+  public void deposit(double amount){
+    acc.deposit(amount);
+    Operation op = new DepositOpr(amount);
+    stack.push(op)
+  }
+  public void undo(){
+    Operation op = stack.pop();
+    op.undo(acc);
+  }
+}
