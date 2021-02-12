c00ade37ff7ff0ce7ca089248f7582bd53fbc34e
getmubarak <getmub@gmail.com>
2021-02-12 16:27:03 +0530

    Create Entry7.java

---
+++ p8/Entry7.java
@@ -0,0 +1,31 @@
+interface Parent{
+}
+class CA implements Parent{
+}
+class CB implements Parent{
+}
+// choose parent at runtime ? 
+class CX  {
+  Parent parent;
+}
+CX o1 = new CX(new CA);
+CX o2 = new CY(new CB);
+
+// lazy load parent
+class CX {
+  Parent parent;
+}
+
+// change parent at runtime
+class CX {
+   Parent parent;
+   
+   if(cond)
+    parent = new CB();
+   ....
+}
+
+// multiple parent
+class CX {
+  List<Parent> parent;
+}
