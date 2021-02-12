c51653ce6f11cbd687942a9969b668d6ccb68ecd
getmubarak <getmub@gmail.com>
2021-02-12 16:41:21 +0530

    Update Entry.java

--- p5/Entry.java
+++ p5/Entry.java
@@ -5,14 +5,16 @@ import java.util.HashMap;
 public class Entry {
     public static void main(String[] args) 
     {
-        Rule r = new Rule("Department","Purchase","==");
-      
+        Rule r1 = new Rule("Department","Purchase","==");
+      	Rule r2 = new Rule("Salary","5000",">");
+	
 	object = new HashMap<>(); 
         object.put("Salary", 3500);
         object.put("Age", 45);
         object.put("Department", "Sales");
         
-        result = r.Eval(object)
-        System.out.print(result);   
+        ans1 = r1.Eval(object);
+	ans2 = r2.Eval(object);
+        System.out.print(ans1 && ans2);   
     }
 }
