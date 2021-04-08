60c1404aba283dcdbd50a1ae6a14e00d12209840
getmubarak <getmub@gmail.com>
2021-04-08 17:13:53 +0530

    Create Entry4A.java

---
+++ p13/Entry4A.java
@@ -0,0 +1,25 @@
+class Ship extends GO{
+  void Collide(GameObject go2){
+    go2.collide(this);
+  }
+  void Collide(Station go2){
+    // logic to  collide ship and station
+  }
+}
+
+class Station extends GO{
+  void Collide(GameObject go2){
+    go2.collide(this);
+  }
+  void Collide(Ship go2){
+    // logic to  collide ship and station
+  }
+}
+
+class Handler
+{
+     public void Collide(GameObject go1,GameObject go2)
+     {
+          go1.Collide(go2);
+     }
+}
