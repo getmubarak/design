ba11bf61ebf97ad55914e8e896ccb8b618b4f8c1
getmubarak <getmub@gmail.com>
2021-04-09 15:57:31 +0530

    Update Entry.java

--- p10/Entry.java
+++ p10/Entry.java
@@ -1,1 +1,34 @@
+@Author :Ankur
 
+interface Enricher{	
+	abstract void do(Account a);
+}
+class OTP implements Enricher{
+	void do(Account a){ .. logic ...}
+}
+class SecretQuestion implements Enricher{
+	void do(Account a){ .. logic }
+}
+class Email implements Enricher {
+	void do(Account a){ .. logic }
+}
+class SMS implements Enricher {
+	void do(Account a){ .. logic }
+}
+public class Account {
+	double balance;
+	double limit=0;
+	List<Enricher> preEnrichers;
+	List<Enricher> postEnrichers;
+    	
+	public void withDraw(double amount) {
+
+		foreach enricher in preEnrichers{
+			enricher.do(this);
+		}
+		balance-=amount;
+		foreach enricher in postEnrichers{
+			enricher.do(this);
+		}
+	}
+}
