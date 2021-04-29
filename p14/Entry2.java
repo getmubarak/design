d7a5ea509fd7b0476ca31b564d0495d1c4c10f8d
getmubarak <getmub@gmail.com>
2021-04-29 16:55:33 +0530

    Update Entry2.java

--- p14/Entry2.java
+++ p14/Entry2.java
@@ -30,13 +30,20 @@ class F3Imp implements Plugin{
   void do(CB){}//3B
   void do(CC){}//3C  
 }
-
+class F4Imp implements Plugin{
+  void do(CA){}//4A
+  void do(CB){}//4B
+  void do(CC){}//4C  
+}
 void doJob(CA a){
-  a.f1(); <--1? A|B|C. (1B)
-  a.f2(); <--2? A|B|C  (2B)
+  a.f1(); <--1? A|B|C. 
+  a.f2(); <--2? A|B|C  
     
   F3Imp f3 = new F3Imp();
-  a.invoke(f3);<--3? A|B|C. (CB::invoke(f3))-->(3B)
+  a.invoke(f3);<--3? A|B|C. 
+  
+  F4Imp f4 = new F4Imp();
+  a.invoke(f4);<--4? A|B|C.
 }
 
 doJob(new ?); <-- CA | CB | CC
