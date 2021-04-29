14dd1c80e050ab244f283ed6e18841fb0ad1a011
getmubarak <getmub@gmail.com>
2021-04-29 14:21:54 +0530

    Update create.java

--- patterns/create.java
+++ patterns/create.java
@@ -1,9 +1,34 @@
-class Factory
+Creator method -> static/global fun
+Factory method -> instance fun (overridable)
+Class factory -> class full of creation logic (no domain logic)
+Abstract factory -> family of class full of creation logic (no domain logic)
+
+class Dept
 {
-   Connection getCon() { ... }
-   Command getCmd() { ... }
-   Transaction getTransaction() { ... }
+   ... domain logic
+      
+   static Emp GetEmp(code){ ... } <-- Creator method
 }
+class Dept
+{
+   ... domain logic
+      
+   Emp GetEmp(code){ ... } <-- factory method
+}
+class Factory <-- class factory
+{
+   Connection getCon() { ... }<-- factory method
+   Command getCmd() { ... }<-- factory method
+   Transaction getTransaction() { ... }<-- factory method
+}
+interface Factory <-- abstract factory
+{
+   Connection getCon();<-- factory method
+   Command getCmd();<-- factory method
+   Transaction getTransaction();<-- factory method
+} 
+   
+
 
 
 class Builder{
