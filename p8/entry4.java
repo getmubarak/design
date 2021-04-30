7eb57a96b05e494c8ed398764b1af88832932b86
getmubarak <getmub@gmail.com>
2021-04-30 13:53:40 +0530

    Create entry4.java

---
+++ p8/entry4.java
@@ -0,0 +1,9 @@
+interface/implements -> get the classes into the same family (abstract programming)
+inheritance/extends -> get the classes into the same family (abstract programming)
+	            + common code
+
+fun(List list){
+	list.remove(3);
+}
+
+fun(stack);
