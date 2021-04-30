bd133d15e805e624a8fdc304a2b62f03a7fe3e8a
getmubarak <getmub@gmail.com>
2021-04-30 14:25:09 +0530

    Update Entry9.java

--- p8/Entry9.java
+++ p8/Entry9.java
@@ -2,25 +2,28 @@ class CA{
 }
 class CB{
 }
-// choose parent at runtime ? 
+//--------------------------------------
+// 1. choose parent at runtime ? 
 class CX extends <runtime> {
 }
 CX o1 = new CX<CA>();
-CX o2 = new CY<CB>();
+CX o2 = new CX<CB>();
 
-// lazy load parent
+//-----------------------------------
+// 2. lazy load parent
 class CX extends CA(?){
 }
 
-// change parent at runtime
+//-----------------------------------
+// 3. change parent at runtime
 class CX extends CA{
    ...
    if(cond)
     parent = new CB();
    ....
 }
-
-// multiple parent
+//-----------------------------------
+// 4. multiple parent
 class CX extends CA, CB {
 }
 
