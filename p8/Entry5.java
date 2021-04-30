12a01929fd19ec5b0337bb23689c88ea2b4ff756
getmubarak <getmub@gmail.com>
2021-04-30 13:58:02 +0530

    Update Entry5.java

--- p8/Entry5.java
+++ p8/Entry5.java
@@ -2,17 +2,17 @@
 class CurrentAccount{
   void withdraw(int amount){
     if(amount < 500)
-      throw
+      throw new InvalidTransactionException();
       
-    //logic
+      //logic for withdrawl
   }
 }
 class OverDraftAccount extends CurrentAccount{
   void withdraw(int amount){
     if(amount < 800)
-      throw
+      throw new InvalidTransactionException();
       
-    //logic2
+      //logic for withdrawl
   }
 }
 
