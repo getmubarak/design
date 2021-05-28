fa1333a89f6e3bcf5d43068eec3e46d6e791810b
getmubarak <getmub@gmail.com>
2021-05-28 15:16:39 +0530

    Update Entry9.java

--- p13/Entry9.java
+++ p13/Entry9.java
@@ -1,9 +1,17 @@
+class Handlers{
+     public void ShipStationCollide(Ship go1,Station go2) {}
+     public void AestroidStationCollide(Aestroid go1,Station go2) {}
+     public void CommetStationCollide(Commet go1,Station go2) {}
+     public void CommetShipCollide(Commet go1,Ship go2) {}
+}
+
 class Handler
 {
      map<key,Lamda> vtable =new map<key,Lamda>();
      void Init() { 
-      vtable.add("ShipStation",()-> invoke logic );
-      vtable.add("ShipShip",()-> invoke logic );
+      Handler handler = new Handler();
+      vtable.add("ShipStation",(go1,go2)-> handler.ShipStationCollide(go1,go2) );
+      vtable.add("ShipShip",(go1,go2)-> handler.ShipShipCollide(go1,go2));
       ...
      }
      public void Collide(GameObject go1,GameObject go2)
