54b40ab0fe83f94e9a248f5eb614c9a7c49bcffe
getmubarak <getmub@gmail.com>
2021-05-28 14:33:51 +0530

    Update Entry4.java

--- p13/Entry4.java
+++ p13/Entry4.java
@@ -1,10 +1,13 @@
 
 class Ship extends GO{
   void Collide(GameObject go2){
-    //go1 is ship
+    //this is ship
     if(go2 is instanceof(Station))
       ...logic of ship with station
-    ...
+    if(go2 is instanceof(Commet))
+      ...logic of ship with commet
+    if(go2 is instanceof(Aestroid))
+      ...logic of ship with aestroid
   }
 }
 
