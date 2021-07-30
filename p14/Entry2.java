a9d79f135353d6ebb4de7b67e990acd2411a6c32
getmubarak <getmub@gmail.com>
2021-07-30 14:22:09 +0530

    Update Entry2.java

--- p14/Entry2.java
+++ p14/Entry2.java
@@ -1,14 +1,11 @@
 interface Plugin{
-  void do(CA);
   void do(CB); 
   void do(CC);  
 }
-class CA{
-  void f1(){} //1A
-  void f2(){} //2A
-  void invoke(Plugin p){
-    p.do(this);
-  }
+interface CA{
+  void f1();
+  void f2();
+  void invoke(Plugin p);
 }
 class CB extends CA{
   void f1(){} //1B
@@ -26,24 +23,22 @@ class CC extends CA{
 }
 //==========================
 class F3Imp implements Plugin{
-  void do(CA){}//3A
   void do(CB){}//3B
   void do(CC){}//3C  
 }
 class F4Imp implements Plugin{
-  void do(CA){}//4A
   void do(CB){}//4B
   void do(CC){}//4C  
 }
 void doJob(CA a){
-  a.f1(); <--1? A|B|C. 
-  a.f2(); <--2? A|B|C  
+  a.f1(); <--1? B|C. 
+  a.f2(); <--2? B|C  
     
   F3Imp f3 = new F3Imp();
-  a.invoke(f3);<--3? A|B|C. 
+  a.invoke(f3);<--3? B|C. 
   
   F4Imp f4 = new F4Imp();
-  a.invoke(f4);<--4? A|B|C.
+  a.invoke(f4);<--4? B|C.
 }
 
-doJob(new ?); <-- CA | CB | CC
+doJob(new ?); <-- CB | CC
