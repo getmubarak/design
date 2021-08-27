abdd1a482aea175f66bd893177e3988f7fee3878
getmubarak <getmub@gmail.com>
2021-08-27 15:39:01 +0530

    Update Entry4A.java

--- p9/Entry4A.java
+++ p9/Entry4A.java
@@ -1,15 +1,14 @@
-interface Plugin
-{
+interface Plugin{
   void do(CA);
   void do(SA);
 }
-clasa Account{
+class Account{
   abstract void invoke(Plugin p);
 }
 -------------------------------------
 class SA extends Account{
  void invoke(Plugin p){
-    p.do(this);
+    p.do(this);//4
   }
 }
 class CA extends Account{
@@ -22,26 +21,23 @@ class CA extends Account{
 public class Factory implements Plugin
 {
   Dialog dlg;
-  public void do(SA a)
-  {
-    dlg= new SADialog();
+  public void do(SA a){
+    dlg= new SADialog();//5
   }
-  public void do(CA a)
-  {
+  public void do(CA a){
     dlg= new CADialog();
   }
 }
 public class Entry {
 	public static void ShowUI(Account account) {
 		Factory factory = new Factory();
-                
-		account.invoke(factory);
-		
+		factory.do(account)<-- error
+     		account.invoke(factory);//3
 		factory.dlg.Display();				
 	}
 	static void main()
 	{
-		Account account = new SA();
-		ShowUI(account);
+		Account account = new SA();//1
+		ShowUI(account);//2
 	}
 }
