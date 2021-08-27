c116ae60611faa0b0b7c3e5c7e4cd7004d819247
getmubarak <getmub@gmail.com>
2021-08-27 16:25:43 +0530

    Update Problem4.java

--- p14/Problem4.java
+++ p14/Problem4.java
@@ -31,7 +31,12 @@ class F3 implements Plugin
   void do(CB b) {} //8
   void do(CC c) {} //9
 }
-
+class F4 implements Plugin
+{
+  void do(CA a) {} //10
+  void do(CB b) {} //11
+  void do(CC c) {} //12
+}
 //----------------------------------
 void do(CA obj){
   obj.f1(); //<- 1 | 3 | 5
@@ -39,5 +44,8 @@ void do(CA obj){
   
   F3 f3 = new F3();
   f3.call(obj); //<- 7| 8 | 9
+  
+  F4 f4 = new F4();
+  f4.call(obj); //<- 10| 11 | 12
   
 }
