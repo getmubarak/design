2d2c1eb21086b157dfc962d1cb209d9fd4bac67d
getmubarak <getmub@gmail.com>
2021-09-06 16:42:01 +0530

    Update Entry.java

--- p25/Entry.java
+++ p25/Entry.java
@@ -6,19 +6,15 @@ class SurveyData:
     
     public void setSavePath(t){
         if t == 0:
-            #raw data.
             path = "c:/application/data/raw.dat";
             hidden = True;
         elif t == 1:
-            #cleaned up data.
             path = "c:/application/data/cleanedUp.dat";
             hidden = True;
         elif t == 2:
-            #processed data.
             path = "c:/application/data/processed.dat";
             hidden = True;
         elif t == 3:
-            #data ready for publication.
             path = "c:/application/data/publication.dat";
             hidden = False;
       }
