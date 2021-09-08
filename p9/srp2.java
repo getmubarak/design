5581f61cfd71d310647e4aa588f42a6d8c1ffb11
getmubarak <getmub@gmail.com>
2021-09-08 15:12:01 +0530

    Create srp2.java

---
+++ p9/srp2.java
@@ -0,0 +1,20 @@
+class CA{}
+class CB extends CA{}
+class CC extends CB{}
+
+class Util{
+  void f(CA){} //1
+  void f(CB){} //2
+  void f(CC){} //3
+}
+
+do(CA a){  
+  Util util = new Util();
+  if(a isinstanceof CC)
+    uitl.f((CC)a); 
+  elseif (a isinstanceof CB)
+     uitl.f((CB)a); 
+  else
+    uitl.f(a); 
+}
+
