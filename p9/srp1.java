353c8c7de96cd871763d1f5d05bd56e652c08f64
getmubarak <getmub@gmail.com>
2021-09-30 15:16:41 +0530

    Create srp1.java

---
+++ p9/srp1.java
@@ -0,0 +1,15 @@
+class CA{}
+class CB extends CA{}
+class CC extends CB{}
+
+class Util{
+  void f(CA){} //1
+  void f(CB){} //2
+  void f(CC){} //3
+}
+
+do(CA a){  
+  Util util = new Util();
+  uitl.f((CC)a); 
+}
+
