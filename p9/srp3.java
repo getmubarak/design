c1a1be4bd56f026e311652b68579745c8b5c8161
getmubarak <getmub@gmail.com>
2021-09-30 16:15:07 +0530

    Update srp3.java

--- p9/srp3.java
+++ p9/srp3.java
@@ -19,11 +19,11 @@ CB b = c; <--upcast
 CA a = c; <--upcast
 
 Util util = new Util();
-//uitl.f(a);
-a.invoke(util);
+//uitl.f(a);<--1
+a.invoke(util);<--3
 
-//uitl.f(b);
-b.invoke(util);
+//uitl.f(b);<--2
+b.invoke(util);<--3
 
-/uitl.f(c);
-c.invoke(util);
+//uitl.f(c);<--3
+c.invoke(util);<--3
