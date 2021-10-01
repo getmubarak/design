b3d1d036a5640438b53570af575cf0eff8d9eeda
getmubarak <getmub@gmail.com>
2021-10-01 16:32:00 +0530

    Update Entry2.java

--- p5/Entry2.java
+++ p5/Entry2.java
@@ -17,7 +17,15 @@ public class Entry {
 	Rule r3 = re.greaterThan("salary","25000");
 	Rule r4 = re.or(r2,r3);
 	Rule rule = re.and(r1,r4);
-	   
+	
+	RuleBuilder rb = new RuleBuilder();
+	rb.addStringEqual("r1","Department","Purchase");
+	rb.addLessThan("r2","Age","28");
+	rb.greaterThan("r3","salary","25000");
+	rb.or("r4","r2","r3");
+	rb.and("r1","r4");    
+	Rule rule = rb.getRule();
+	    
 	Rule rule = re.and(re.stringEqual("Department","Purchase"),
 			   re.or(re.lessThan("Age","28"),
 				 re.greaterThan("salary","25000")
@@ -25,6 +33,8 @@ public class Entry {
    
 	res = rule.eval(object);	   
         System.out.print(ans);  
+			   
+	
 	        
     }
 }
