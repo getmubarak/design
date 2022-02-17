2985f259ad8be3d8ac528d94a85940bb0aac2547
getmubarak <getmub@gmail.com>
2022-02-17 15:11:46 +0530

    Create test0.java

---
+++ p11/test0.java
@@ -0,0 +1,24 @@
+interface Broker{
+	void trade();
+}
+class BrokerImp{
+ void trade(){ ... logic ... }
+}
+class BrokerFactory{
+  Broker getInstance(){
+    return new BrokerImp();
+  }
+}
+public class Stock {
+	double rate;
+	Broker broker;
+	Stock(){
+    BrokerFactory f = new BrokerFactory();
+		this.broker = f.getInstance();
+	}
+	public void changeRate(double newRate) {
+		rate = newRate;
+		//if(condition && broker != null)
+			broker.trade();
+	}
+}
