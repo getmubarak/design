01fd7cb3724b2d3b643448eaacbcb273a7c9d320
getmubarak <getmub@gmail.com>
2022-03-10 16:40:30 +0530

    Update Entry4.java

--- p9/Entry4.java
+++ p9/Entry4.java
@@ -15,29 +15,22 @@ class CA extends Account{
   }
 }
 -------------------------------------
-
-public class Factory 
-{
+public class Factory {
   Dialog dlg;
-  public void do(SA a)
-  {
+  public void do(SA a){
     dlg= new SADialog();
   }
-  public void do(CA a)
-  {
+  public void do(CA a){
     dlg= new CADialog();
   }
 }
 public class Entry {
 	public static void ShowUI(Account account) {
-		Factory factory = new Factory();
-                
+		Factory factory = new Factory();            
 		account.invoke(factory);
-		
 		factory.dlg.Display();				
 	}
-	static void main()
-	{
+	static void main(){
 		Account account = new SA();
 		ShowUI(account);
 	}
