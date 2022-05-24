095bab39e70fcdf98c70846b0afd7c50611b64f1
getmubarak <getmub@gmail.com>
2022-05-24 15:22:15 +0530

    Update Closure.java

--- p2/solutions/Closure.java
+++ p2/solutions/Closure.java
@@ -7,17 +7,19 @@ Lamda fun(int x){
   # x,z is the clousre
   
   return (y)-> x + y; 
+  /*
          fun2(y){
             return x+ y;
          }
+  */
 }
 
 void main(){
   Lamda f1 = fun(10);  
-  f1{x:10,z:15,logic(y)}
+  //f1{x:10,z:15,logic(y)}
   
   Lamda f2 = fun(20);
-  f2{x:20,z:25,logic(y)}
+  //f2{x:20,z:25,logic(y)}
   
   int res = f1(15); #25
   res = f2(10); #30
