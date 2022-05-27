4d116555abf9c4ccc343a2f288c74c38fd61122a
getmubarak <getmub@gmail.com>
2022-05-27 16:17:37 +0530

    Update structure.java

--- patterns/structure.java
+++ patterns/structure.java
@@ -4,18 +4,18 @@ class Emp{
   Emp ref;
 }
 
-//tree
+//tree of same class objects
 class Emp{
   List<Emp> emps;
 }
 
-// Linked List
+// Linked List of family members <-- Decorator , Chain of responsibility 
 interface IEmp{}
 class Emp implements IEmp {
   IEmp ref;
 }
 
-//tree
+//tree of family members <-- composite pattern
 interface IEmp{}
 class Emp{
   List<IEmp> emps;
