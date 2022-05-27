613554b7d1b2a84b3be35b70834b879dd65f0645
getmubarak <getmub@gmail.com>
2022-05-27 16:15:37 +0530

    Create structure.java

---
+++ patterns/structure.java
@@ -0,0 +1,22 @@
+// linked list
+
+class Emp{
+  Emp ref;
+}
+
+//tree
+class Emp{
+  List<Emp> emps;
+}
+
+// Linked List
+interface IEmp{}
+class Emp implements IEmp {
+  IEmp ref;
+}
+
+//tree
+interface IEmp{}
+class Emp{
+  List<IEmp> emps;
+}
