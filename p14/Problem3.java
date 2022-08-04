2236b00963de30d26554bd1597b1c087edd7c2d0
getmubarak <getmub@gmail.com>
2022-08-04 16:03:27 +0530

    Update Problem3.java

--- p14/Problem3.java
+++ p14/Problem3.java
@@ -2,21 +2,21 @@
 class CA{
   void f1(){} //1
   void f2(){} //2
-  void call(CX x){
+  void call(CX x){ // 10
     x.f3(this);
   }
 }
 class CB extends CA{
   void f1(){} //3
   void f2(){} //4
-  void call(CX x){
+  void call(CX x){ // 11
     x.f3(this);
   }
 }
 class CC extends CA{
   void f1(){} //5
   void f2(){} //6
-  void call(CX x){
+  void call(CX x){ // 12
     x.f3(this);
   }
 }
