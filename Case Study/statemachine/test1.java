99556e2258c4cd89e807c99fa3cc7e8adda8d7a1
getmubarak <getmub@gmail.com>
2022-11-02 15:52:15 +0530

    Update test1.java

--- Case Study/statemachine/test1.java
+++ Case Study/statemachine/test1.java
@@ -12,23 +12,26 @@ class StateMachineAuthoringTest{
   void createBasicWorkflowTest(){
     StateMachine sm = new StateMachine();
     
-    State s1 = new State("Idle");
-    State s2 = new State("Running");
-    State s3 = new State("Suspended");
+    Action a1 = new EmailAction(...);
+    ...
     
-    sm.add(s1);
-    sm.add(s2);
-    sm.add(s3);
+    State s1 = new State("Idle",a1,a2,);
+    State s2 = new State("Running",a3,null?);
+    State s3 = new State("Suspended",null?,null?);
     
-    Transition t1 = new Transition("start",s2);
-    Transition t2 = new Transition("pause",s3);
-    Transition t3 = new Transition("stop",s1);
-    Transition t4 = new Transition("resume",s2);
+    sm.add(s1)
+      .add(s2)
+      .add(s3);
+    
+    Transition t1 = new Transition("start",s2,a4);
+    Transition t2 = new Transition("pause",s3,null?);
+    Transition t3 = new Transition("stop",s1,a5);
+    Transition t4 = new Transition("resume",s2,null);
     
     s1.add(t1);
-    s2.add(t2);
-    s2.add(t3);
-    s3.add(t3);
-    s3.add(t4);
+    s2.add(t2)
+      .add(t3);
+    s3.add(t3)
+      .add(t4);
   }
 }
