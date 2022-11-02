d41b29edf5bc279ed934c5d675afd0a43cf75ee6
getmubarak <getmub@gmail.com>
2022-11-02 16:53:16 +0530

    Update test1.java

--- Case Study/statemachine/test1.java
+++ Case Study/statemachine/test1.java
@@ -8,30 +8,66 @@ StopWatch
   # resume : running
   # stop  : Idle
 
+class DomainEvent{
+  int eventId;
+  map<String,String> properties;
+}
+class State{
+  String StateId;
+  Action onExit;
+  Action onEntry;
+  map<String,Transition> transitions;
+  map<String,EventType> events;
+  
+  void Add(Transition t){
+    transitions.put(t.getEventId(),t);
+  }
+  void Add(EventType et){
+   events.put(et.getEventId(),et);
+  }
+  State FireEvent(DomainEvent event){
+    EventType et=events.get(event.getId());
+    et.executeAction();
+    Transition t=transitions.get(event.getId());
+    onExit();
+    t.state.OnEnter();
+    return t.state;
+  }
+}
+class StateMachineInstance{
+  State current = new State("Initial");
+  
+  void FireEvent(DomainEvent event){
+    current = current.FireEvent(event);
+  }
+}
 class StateMachineAuthoringTest{
   void createBasicWorkflowTest(){
-    StateMachine sm = new StateMachine();
+    StateMachineDefinition sm = new StateMachineDefinition();
     
+    Action a0 = new NullAction(...);
     Action a1 = new EmailAction(...);
-    ...
     
-    State s1 = new State("Idle",a1,a2,);
-    State s2 = new State("Running",a3,null?);
-    State s3 = new State("Suspended",null?,null?);
+    State s1 = new State("Idle",a1,a2);
+    State s2 = new State("Running",a3,a0);
+    State s3 = new State("Suspended",a0,a0);
     
     sm.add(s1)
       .add(s2)
       .add(s3);
+      
+    s1.add(new Transition("START",s2));
+    s2.add(new Transition("PAUSE",s3))
+      .add(new Transition("STOP",s1));
+    s3.add(new Transition("STOP",s1))
+      .add(new Transition("RESUME",s2));
+      
+    s1.add(new EventType("START",a4));
+    s2.add(new EventType("STOP",a0));
     
-    Transition t1 = new Transition("start",s2,a4);
-    Transition t2 = new Transition("pause",s3,null?);
-    Transition t3 = new Transition("stop",s1,a5);
-    Transition t4 = new Transition("resume",s2,null);
-    
-    s1.add(t1);
-    s2.add(t2)
-      .add(t3);
-    s3.add(t3)
-      .add(t4);
+    sm.add(new Transition("CREATE",s1,a0));
+    XmlSerializer sr = new XmlSeralizer();
+    sr.serialize(sm);
   }
+  
 }
