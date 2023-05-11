920abf9d472bb6c4089217cb17859f5b34aa84e3
getmubarak <getmub@gmail.com>
2023-05-11 15:32:04 +0530

    Update Entry4.java

--- p3/Entry4.java
+++ p3/Entry4.java
@@ -5,23 +5,22 @@ public class Entry {
     Entry entry = new Entry();
     entry.do(new DIEngine());
   }
-  public void do(,,,Command cmd2,
-                Transaction transaction2,Command cmd3 ){  
-		Connection connection = DI.get<Connection>();
-    connection.open("mydb;scott;tiger");
-    Transaction transaction = DI.get<Transaction>();
-		transaction.begin(connection);
-    Command cmd1 = DI.get<Command>();
-		cmd1.execute(transaction, "insert into emp values(10,'jack',2500')");
+  public void do(DIEngine DI){  
+	Connection connection = DI.get<Connection>();
+    	connection.open("mydb;scott;tiger");
+    	Transaction transaction = DI.get<Transaction>();
+	transaction.begin(connection);
+    	Command cmd1 = DI.get<Command>();
+	cmd1.execute(transaction, "insert into emp values(10,'jack',2500')");
   	Command cmd2 = DI.get<Command>();
-		cmd2.execute(transaction, "insert into emp values(20,'jill',4300')");
-		transaction.commit();
-		Transaction transaction2 = DI.get<Transaction>();
-		transaction2.begin(connection);
-		Command cmd3 = DI.get<Command>();
-		cmd3.execute(transaction, "insert into emp values(10,'jack',2500')");
+	cmd2.execute(transaction, "insert into emp values(20,'jill',4300')");
+	transaction.commit();
+	Transaction transaction2 = DI.get<Transaction>();
+	transaction2.begin(connection);
+	Command cmd3 = DI.get<Command>();
+	cmd3.execute(transaction, "insert into emp values(10,'jack',2500')");
   	transaction2.commit();
-		connection.close();
+	connection.close();
 		
-	}
+  }
 }
