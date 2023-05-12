fa9757a9487348b38d879bbe569da3b78175bafc
getmubarak <getmub@gmail.com>
2023-05-12 15:33:38 +0530

    Create Entry5.java

---
+++ p5/Entry5.java
@@ -0,0 +1,78 @@
+
+interface Rule
+{
+  bool eval(HashMap<string,string> obj);
+}
+class GreaterRule implements Rule{
+  string property;
+  double value;
+  public GreaterRule(string property,double value){
+    this.property = property;
+    this.value = value;
+  }
+  bool eval(HashMap<string,object> obj){
+    cur_value = obj.get(property);
+    if(double.parse(cur_value) > value)
+      return true;
+    else
+      return false;
+  }
+}
+class LesserRule implements Rule{
+  string property;
+  double value;
+  public LesserRule(string property,double value){
+    this.property = property;
+    this.value = value;
+  }
+  bool eval(HashMap<string,object> obj){
+    cur_value = obj.get(property);
+    if(double.parse(cur_value) < value)
+      return true;
+    else
+      return false;
+  }
+}
+class EqualStringRule implements Rule{
+  string property;
+  string value;
+  public EqualStringRule(string property,string value){
+    this.property = property;
+    this.value = value;
+  }
+  bool eval(HashMap<string,object> obj){
+    cur_value = obj.get(property);
+    if(cur_value.equals(value))
+      return true;
+    else
+      return false;
+  }
+}
+class AndRule implements Rule{
+  Rule lhs;
+  Rule rhs;
+  public AndRule(Rule lhs,Rule rhs){
+    this.lhs = lhs;
+    this.rhs = rhs;
+  }
+  bool eval(HashMap<string,object> obj){
+    return lhs.eval(obj) && rhs.eval(obj);
+  }
+}
+
+
+HashMap<string,string> object = new HashMap<>(); 
+        object.put("Salary", 3500);
+        object.put("Age", 45);
+        object.put("Department", "Sales");
+        
+        //Composite Pattern
+        Rule r1 = new GreaterRule("Salary",5000.00);
+	Rule r2 = new LesserRule("Age",50);
+	Rule r3 = new EqualStringRule("Department","Purchase");
+	Rule r4 = new AndRule(r1,r2);
+	Rule r5 = new OrRule(r4,r3);    
+	    
+	res = r5.eval(object);	
+	//"((Salary > 5000 && Age < 50) || (Department == 'Purchase'))"    
+        System.out.print(res);   
