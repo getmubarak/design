10a8f21b227e0f026c6042ffd7bdbbda798cd35c
getmubarak <getmub@gmail.com>
2023-06-09 15:18:55 +0530

    Update Entry4.java

--- p5/Entry4.java
+++ p5/Entry4.java
@@ -12,8 +12,8 @@ public class Entry {
         
         //((Department == 'purchase') and (age < 28 or salary > 25000))
         Rule r1 = new StringEqual("Department","Purchase");
-	      Rule r2 = new LesserThan("Age","28");
-	      Rule r3 = new GreaterThan("salary","25000");
+	Rule r2 = new LesserThan("Age","28");
+	Rule r3 = new GreaterThan("salary","25000");
         Rule r4 = new OrRule(r2,r3);
         Rule r5 = new AndRule(r1,r4);
         
@@ -21,7 +21,7 @@ public class Entry {
              ->r4->r2
                  ->r3
                
-	      ans = r5.Eval(object);
+	ans = r5.Eval(object);
         System.out.print(ans);  
 	        
     }
