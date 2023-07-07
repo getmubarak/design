1db805136def92f037d9952eba159f8751ca7e63
getmubarak <getmub@gmail.com>
2023-07-07 14:13:07 +0530

    Update srp0.java

--- p9/srp0.java
+++ p9/srp0.java
@@ -16,3 +16,25 @@ uitl.f(b);
 uitl.f(c);
 
 1,1,1.     1,2,3.    3,3,3.
+
+
+
+class CX{
+  void f() {}//1
+}
+class CY. extends CX {
+  void f() {}//2
+}
+class CZ. extends CY {
+  void f() {}//3
+}
+
+CZ z = new CZ();
+CY y = z;
+CX x = y;
+
+x.f();
+y.f();
+z.f();
+
+1,1,1.     1,2,3.    3,3,3.
