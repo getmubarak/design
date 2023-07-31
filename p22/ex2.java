2add31f7ac69e5a1a260a7f3fa4eb4140d13481d
getmubarak <getmub@gmail.com>
2023-07-31 15:28:22 +0530

    Create ex2.java

---
+++ p22/ex2.java
@@ -0,0 +1,20 @@
+class Shape{}
+class Rect extends Shape {}
+class Square extends Rect {}
+
+class Util{
+  void fun(Shape shape){} //1
+  void fun(Rect rect){} //2
+  void fun(Square square){} //3
+};
+
+
+void DoJob(Shape shape){
+  Util util = new Util();
+  util.fun(shape);
+}
+
+void main(){
+  DoJob(new ...);
+}
+//1. //2 //3
