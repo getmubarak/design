afaab0ed3f8df4a2ee55e9e792fb2d52ec86f78e
getmubarak <getmub@gmail.com>
2023-11-29 14:18:21 +0530

    Create Message4.java

---
+++ p21/Message4.java
@@ -0,0 +1,33 @@
+interface Encryption{
+  void encrypt(string data);
+  string decrypt(string cipher);
+}
+
+class Aes implements Encryption{ ... }
+class BlowFish implements Encryption{ ... }
+
+public class Message {
+    private Encryption algorithm;
+    private String text;
+    public Message(string algo){
+        EncryptionFactory factory = new EncryptionFactory();
+        this.algorithm=factory.create();
+   }
+   public string getText(){
+        return algorithm.decrypt(text);
+   }
+   public void setText(String text){
+         text = algorithm.encrypt(text);
+   }     
+}
+class EncryptionFactory{
+  Encryption create(string algo){
+        if (algo.equals("Aes")){
+            return new Aes();
+        }
+       else if (algo.equals("Blowfish")){
+           return new BlowFish();
+        }
+        return null;
+   }
+}
