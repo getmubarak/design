d3799c269199ac34bd54b8a056cc67d48a8911b8
getmubarak <getmub@gmail.com>
2024-08-08 21:37:28 -0700

    Create Entry1A.java

---
+++ p9/Entry1A.java
@@ -0,0 +1,36 @@
+package problem9;
+
+interface Account{}
+class CA : Account{}
+class SA : Account{}
+
+interface Dialog {
+	void Display();
+}
+class SADialog : Dialog {}
+class CADialog : Dialog {}
+
+public class DialogFactory{
+	public Dialog CreateUI(Account account){
+		Dialog dlg=null;
+		
+		if(account instanceof SA) {
+			dlg = new SADialog();
+		}else if(account instanceof CA) {
+			dlg = new CADialog();
+		}else{
+      throw new InvalidDomainObjectExpection();
+    }
+
+		return new dlg;
+	}
+}
+
+public class Entry {
+	static void main(){
+		Account account = new SA();
+		DialogFactory factory = new DialogFactory();
+		Dialog dlg=factory.createUI(account);
+		dlg.Display();			
+	}
+}
