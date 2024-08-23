155b2aa25095d585536ee5bfeaf87f57bd4bc648
getmubarak <getmub@gmail.com>
2024-08-22 21:21:39 -0700

    Update and rename Entry.java to Entry1.java

--- p7/Entry.java
+++
@@ -1,5 +0,0 @@
-package problem7;
-
-public class Entry {
-
-}
---
+++ p7/Entry1.java
@@ -0,0 +1,18 @@
+package problem7;
+
+
+Account{}
+class SA : Account{}
+class CA : Account{}
+
+class SilverSA : SA{}
+class PlatinumSA : SA{}
+class BronzeSA : SA{}
+
+class SilverCA : CA{}
+class PlatinumCA : CA{}
+class BronzeCA : CA{}
+
+class SilverOverdraft : Overdraft{}
+class PlatinumOverdraft :Overdraft{}
+class BronzeOverdraft : Overdraft{}
