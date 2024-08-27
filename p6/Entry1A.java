073d48d77652ee88adaf1afe9bd1aae3f7044d1a
getmubarak <getmub@gmail.com>
2024-08-27 00:09:47 -0700

    Create Entry1A.java

---
+++ p6/Entry1A.java
@@ -0,0 +1,46 @@
+package problem6;
+
+public class Invoice {
+	private int taxType;
+	
+	void setTaxType(int taxType) {
+		this.taxType = taxType;
+	}
+	bool IsAmountHigherThanAllowedThreshHold(int amount){
+  	if(amount > 1000)
+      return true;
+    else
+      return false;
+  }
+	public double getTotal(){
+		double amount = getSubtotal();
+		switch(taxType)
+		{
+			case 1:
+				if(IsAmountHigherThanAllowedThreshHold(amount))
+					amount += amount * 0.05;
+				break;
+			case 2:
+				amount += amount * 0.025 + 500;
+				break;
+			case 3:
+				if(IsAmountLowerThanAllowedThreshHold(amount))
+					amount += (amount- 5000) * 0.3;
+				else
+					amount += (amount- 5000) * 0.4;
+				break;
+		}
+		return amount;
+	}
+	double getSubtotal()
+	{
+		double subtotal = 0;
+		/* 
+		 * foreach(lineItem in Items)
+		 * {
+		 * 		subtotal += lineItem.getPrice() * lineItem.Qty()
+		 * }
+		 */
+		return subtotal;
+	}
+}
