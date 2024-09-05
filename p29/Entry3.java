3c592e05fcb96af5bf19b36f9fb3bac809612e01
getmubarak <getmub@gmail.com>
2024-09-04 23:30:58 -0700

    Create Entry3.java

---
+++ p29/Entry3.java
@@ -0,0 +1,63 @@
+public class Repository<T> where T : class
+{
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public T GetById(int id)
+    {
+        return _dbSet.Find(id);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _dbSet.ToList();
+    }
+
+    public void Add(T entity)
+    {
+        _dbSet.Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+        _context.SaveChanges();
+    }
+
+    public void Delete(T entity)
+    {
+        if (_context.Entry(entity).State == EntityState.Detached)
+        {
+            _dbSet.Attach(entity);
+        }
+        _dbSet.Remove(entity);
+        _context.SaveChanges();
+    }
+}
+
+public class CustomerRepository : Repository<Customer>
+{
+    public CustomerRepository(DbContext context) : base(context) { }
+}
+
+public class OrderRepository : Repository<Order>
+{
+    public OrderRepository(DbContext context) : base(context) { }
+}
+
+public class ProductRepository : Repository<Product>
+{
+    public ProductRepository(DbContext context) : base(context) { }
+}
+
+
+
+
