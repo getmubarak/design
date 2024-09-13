7127cce9995f4f6d097ac7aa350d749f9d0de951
getmubarak <getmub@gmail.com>
2024-09-12 21:30:52 -0700

    Update Entry4.py

--- p13/Entry4.py
+++ p13/Entry4.py
@@ -10,6 +10,18 @@ class Ship:
         elif isinstance(other, Asteroid):
             # Logic of Ship with Asteroid
             pass
+class Station:
+    def collide(self, other):
+        # This is Station
+        if isinstance(other, Ship):
+            # Logic of Ship with Station
+            pass
+        elif isinstance(other, Comet):
+            # Logic of Station with Comet
+            pass
+        elif isinstance(other, Asteroid):
+            # Logic of Station with Asteroid
+            pass
 
 class Handler:
     def collide(self, go1, go2):
