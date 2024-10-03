550f0798f703831e6febc6df437634c3548ef197
getmubarak <getmub@gmail.com>
2024-10-02 22:57:05 -0700

    Update Entry4.java

--- p9/Entry4.java
+++ p9/Entry4.java
@@ -1,26 +1,26 @@
 Entry2 + Srp2 = Entry4
 
 class Account{
-  abstract void invoke(Factory f);
+  abstract void handle(Factory f);
 }
 -------------------------------------
 class SA extends Account{
- void invoke(Factory f){
-    f.do(this);
+ void handle(Factory f){
+    f.doSA(this);
   }
 }
 class CA extends Account{
- void invoke(Factory f){
-    f.do(this);
+ void handle(Factory f){
+    f.doCA(this);
   }
 }
 -------------------------------------
 public class Factory {
   Dialog dlg;
-  public void do(SA a){
+  public void doSA(SA a){
     dlg= new SADialog();
   }
-  public void do(CA a){
+  public void doCA(CA a){
     dlg= new CADialog();
   }
 }
