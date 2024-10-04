fdd49af1b4e386618ffe44870a374a79bfaf3383
getmubarak <getmub@gmail.com>
2024-10-03 22:11:43 -0700

    Create ex1.java

---
+++ p5/ex1.java
@@ -0,0 +1,22 @@
+package problem5;
+
+import java.util.HashMap;
+
+//requirement
+//===================
+//object : { "Salary": 3500  , "Age" : 45 ,  "Department": "Sales" }
+//rule : Department == 'purchase'
+
+public class Entry {
+    public static void main(String[] args) 
+    {
+        object = new HashMap<>(); 
+        object.put("Salary", 3500);
+        object.put("Age", 45);
+        object.put("Department", "Sales");
+        
+        Rule r1 = new Rule("Department","==","Purchase");
+      	ans1 = r1.Eval(object);
+        System.out.print(ans);  
+    }
+}
