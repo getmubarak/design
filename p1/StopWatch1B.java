04f84c011d2af446aedb9f0d8d4a6ed2e6e12006
getmubarak <getmub@gmail.com>
2025-03-24 21:35:43 -0700

    Create StopWatch1B.java

---
+++ p1/StopWatch1B.java
@@ -0,0 +1,52 @@
+package problem1;
+
+public class StopWatch {
+	int state = 0;
+	const int IDLE=0;
+	const int RUNNING=1;
+	const int SUSPENDED=2;
+	
+	
+	public boolean start()
+	{
+		if(state == RUNNING)
+			return false;
+		if(state == SUSPENDED)
+			return false;
+    if(state == IDLE)
+  		//logic
+  		state = RUNNING;
+  		return true;
+	}
+	public boolean stop()
+	{
+    if(state == IDLE)
+      return false;
+		if(state == RUNNING || state == SUSPENDED)
+	  	//logic
+		  state = IDLE;
+		  return true;
+	}
+	public boolean pause()
+	{
+    if(state == IDLE)
+			return false;
+		if(state == SUSPENDED)
+			return false;
+		if(state == RUNNING)
+  		//logic
+  		state = SUSPENDED;
+  		return true;
+	}
+	public boolean resume()
+	{
+    if(state == RUNNING)
+			return false;
+		if(state == IDLE)
+			return false;
+   	if(state == SUSPENDED)
+      //logic
+  		state = RUNNING;
+  		return true;
+	}
+}
