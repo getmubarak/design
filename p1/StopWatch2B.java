732996712afd747726abf608293322e05e59de84
getmubarak <getmub@gmail.com>
2025-03-24 21:46:35 -0700

    Create StopWatch2B.java

---
+++ p1/StopWatch2B.java
@@ -0,0 +1,42 @@
+public class StopWatch {
+	int state = 0;
+	const int IDLE=0;
+	const int RUNNING=1;
+	const int SUSPENDED=2;
+	
+	
+	public void start()
+	{
+		if(state == RUNNING)
+			throw new InvalidStateException();
+		if(state == SUSPENDED)
+			throw new InvalidStateException();
+		if(state == IDLE)		
+  		//logic
+  		state = RUNNING;
+	}
+	public void stop()
+	{
+		if(state != RUNNING || state!= SUSPENDED)
+			throw new InvalidStateException();
+		
+		//logic
+		state = IDLE;
+	}
+	public void pause()
+	{
+		if(state != RUNNING)
+			throw new InvalidStateException();
+		
+		//logic
+		state = SUSPENDED;
+	}
+	public void resume()
+	{
+		if(state != SUSPENDED)
+			throw new InvalidStateException();
+		
+		//logic
+		state = RUNNING;
+	}
+}
