ccaa004046034d396cab55ee50867e2da48326a3
getmubarak <getmub@gmail.com>
2025-06-26 14:57:33 -0700

    Update Entry1.java

--- p7/Entry1.java
+++ p7/Entry1.java
@@ -1,24 +1,52 @@
-interface EligibilityCheck {
-    boolean evaluate(Customer customer);
+class LoanEligibility {
+    boolean isEligible(Customer c){
+        return checkKYC(c) && checkCreditScore(c);
+    }
+    
+    boolean checkKYC(Customer c) { ... }
+    boolean checkCreditScore(Customer c) { ... }
 }
 
-class KYCCheck implements EligibilityCheck { ... }
-class CreditScoreCheck implements EligibilityCheck { ... }
-class CollateralCheck implements EligibilityCheck { ... }
-class AadhaarCheck implements EligibilityCheck { ... }
-class SSNCheck implements EligibilityCheck { ... }
+class LoanEligibilitySalaried extends LoanEligibility {
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkSalarySlips(c);
+    }
+    boolean checkSalarySlips(Customer c) { ... }
+}
 
+class LoanEligibilitySelfEmployed extends LoanEligibility {
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkBusinessDocuments(c);
+    }
+    boolean checkBusinessDocuments(Customer c) { ... }
+}
 
-List<EligibilityCheck> indiaHomeLoanChecks = List.of(
-    new KYCCheck(),
-    new CreditScoreCheck(),
-    new CollateralCheck(),
-    new AadhaarCheck()
-);
+class HomeLoanEligibilitySalaried extends LoanEligibilitySalaried {
+    @Override
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkCollateral(c);
+    }
+    boolean checkCollateral(Customer c) { ... }
+}
+class HomeLoanEligibilitySelfEmployed extends LoanEligibilitySelfEmployed {
+    @Override
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkCollateral(c);
+    }
+    boolean checkCollateral(Customer c) { ... }
+}
 
-List<EligibilityCheck> usHomeLoanChecks = List.of(
-    new KYCCheck(),
-    new CreditScoreCheck(),
-    new CollateralCheck(),
-    new SSNCheck()
-);
+class PersonalLoanEligibilitySalaried extends LoanEligibilitySalaried {
+    @Override
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkIncome(c);
+    }
+    boolean checkIncome(Customer c) { ... }
+}
+class PersonalLoanEligibilitySelfEmployed extends LoanEligibilitySelfEmployed {
+    @Override
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkIncome(c);
+    }
+    boolean checkIncome(Customer c) { ... }
+}
