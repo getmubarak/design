43d0d258c3ec46d9db10efa9f71f05e383fd8e4e
getmubarak <getmub@gmail.com>
2025-07-08 23:46:58 -0700

    Update Client3.java

--- p24/Client3.java
+++ p24/Client3.java
@@ -1,12 +1,27 @@
+# library
+public interface Account{
+      void withdraw();
+      void deposit();
+}
+public class AccountImp implements Account
+{
+    public void withdraw() { //logic
+    }
+    public void deposit()  { //logic
+    }
+}
+
+#client
+
 class Client{
-      public void do(IX ix){
-          ix.f1();
-          ix.f2();
+      public void do(Account acc){
+          acc.withdraw();
+          acc.deposit();
       }
       public static void main()
       {
           Client client = new Client();
-          client.do(new CA());
+          client.do(new AccountImp());
       }
 }
 
