83d5301ef73bd9339ab57bc898de816ead09b0c9
getmubarak <getmub@gmail.com>
2025-08-07 22:00:04 -0700

    Update Entry5.java

--- p7/Entry5.java
+++ p7/Entry5.java
@@ -9,24 +9,44 @@ class CollateralCheck implements EligibilityCheck { ... }
 class AadhaarCheck implements EligibilityCheck { ... }
 class SSNCheck implements EligibilityCheck { ... }
 
+class Factory{
+    DecisionTable table = new DecisionTable();
+    Factory(){
+        table.add(LoanType.HomeLoan, IncomeType.Salaried, getElibilitiesForHomeLoanSalaried());
+        table.add(LoanType.HomeLoan, IncomeType.Business, getElibilitiesForHomeLoanBusiness());
+        table.add(LoanType.PersonelLoan, IncomeType.Salaried, getElibilitiesForPersonelLoanSalaried());
+        table.add(LoanType.PersonelLoan, IncomeType.Business, getElibilitiesForPersonelLoanBusiness());
+    }
+    public getElibilitieChecks(LoanType, IncomeType){
+        return table.get(LoanType,IncomeType);
+    }
+    private List<EligibilityCheck> getElibilitiesForHomeLoanSalaried(){
+        return List.of(
+            new KYCCheck(),
+            new CreditScoreCheck(),
+            new SalarySlipCheck(),
+            new AadhaarCheck()
+        );
+    }
+    private List<EligibilityCheck> getElibilitiesForHomeLoanBusiness(){    
+        return List.of(
+            new KYCCheck(),
+            new CreditScoreCheck(),
+            new CollateralCheck(),
+            new AadhaarCheck()
+        );
+     }
+    private List<EligibilityCheck> getElibilitiesForPersonelLoanBusiness();
+}
 
-List<EligibilityCheck> personelSalaried = List.of(
-    new KYCCheck(),
-    new CreditScoreCheck(),
-    new SalarySlipCheck(),
-    new AadhaarCheck()
-);
-List<EligibilityCheck> personelBusiness = List.of(
-    new KYCCheck(),
-    new CreditScoreCheck(),
-    new CollateralCheck(),
-    new AadhaarCheck()
-);
-
-
-class EligibilityEngine {
-    List<EligibilityCheck> checks;
-    boolean evaluate(Customer c) {
-        return checks.stream().allMatch(check -> check.evaluate(c));
+class Client{
+    void Main()
+    {
+        Customer customer ...;
+        LoanRequest loanRequet = ...
+        Factory factory = new Factory();
+        List<EligibilityCheck> checks= factory.getElibilitieChecks(loanRequet.LoanType, customer.IncomeType);
+        
+        bool resutl= checks.stream().allMatch(check -> check.evaluate(c));
     }
 }
