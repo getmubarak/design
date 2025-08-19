cd2d349b428406d914cfa98636f068018ab37220
getmubarak <getmub@gmail.com>
2025-08-18 23:02:34 -0700

    Update ex3.java

--- p22/ex3.java
+++ p22/ex3.java
@@ -1,31 +1,31 @@
 class Shape{
-   virtual fun(){} \\A1
+   virtual rotate(){} \\A1
 };
 class Rect extends Shape {
-   fun(){} \\A2
+   rotate(){} \\A2
 }
 class Square extends Rect {
-   fun(){} \\A3  
+   rotate(){} \\A3  
 };
 
 class Util{
-  void fun(Shape shape){} //B1
-  void fun(Rect rect){} //B2
-  void fun(Square square){} //B3
+  void fill(Shape shape){} //B1
+  void fill(Rect rect){} //B2
+  void fill(Square square){} //B3
 };
 void main(){
   Square square = new Square();
   Rect rect = square; 
   Shape shape = rect; 
 // dynamic / runtime / over riding
-  shape.fun();
-  rect.fun();
-  square.fun();
+  shape.rotate();
+  rect.rotate();
+  square.rotate();
 // static / compile time/ over loading
   Util util = new Util(); 
-  util.fun(shape);
-  util.fun(rect);
-  util.fun(square); 
+  util.fill(shape);
+  util.fill(rect);
+  util.fill(square); 
 }
 //A1A2A3 //A3A3A3 //A1A1A1
 //B1B2B3 //B3B3B3 //B1B1B1
