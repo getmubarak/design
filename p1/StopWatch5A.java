d8cefbddb5b5468fc068090e0db77833c57c2c5f
getmubarak <getmub@gmail.com>
2025-08-19 22:29:30 -0700

    Update StopWatch5A.java

--- p1/StopWatch5A.java
+++ p1/StopWatch5A.java
@@ -8,20 +8,20 @@ interface State{
 }
 class Idle implements State{
 	void Start() { //logic 	}
-        void Pause() { throw new InvalidStateException(); }
-        void Resume() { throw new InvalidStateException(); }
+    void Pause() { throw new InvalidStateException(); }
+    void Resume() { throw new InvalidStateException(); }
 	void Stop() { throw new InvalidStateException(); }
 }
 class Running implements State{
 	void Start() { throw new InvalidStateException(); }
-        void Pause() { // logic }
-        void Resume() { throw new InvalidStateException(); }
+    void Pause() { // logic }
+    void Resume() { throw new InvalidStateException(); }
 	void Stop() { //logic }
 }
 class Suspended implements State{
 	void Start() { throw new InvalidStateException(); }
-        void Pause() { throw new InvalidStateException(); }
-        void Resume() { //logic }
+    void Pause() { throw new InvalidStateException(); }
+    void Resume() { //logic }
 	void Stop() { //logic }
 }
 public class StopWatch {
