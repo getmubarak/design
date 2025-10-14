45955d048406f525c05ddacf09abf85dbd10a807
getmubarak <getmub@gmail.com>
2025-10-13 23:56:39 -0700

    Update Entry3.java

--- p13/Entry3.java
+++ p13/Entry3.java
@@ -1,16 +1,17 @@
 class Handler
 {
-     public void Collide(Ship go1,Station go2) {}
-     public void Collide(Aestroid go1,Station go2) {}
-     public void Collide(Commet go1,Station go2) {}
-     public void Collide(Commet go1,Ship go2) {}
+     public void Invoke(Ship go1,Station go2) {}
+     public void Invoke(Aestroid go1,Station go2) {}
+     public void Invoke(Commet go1,Station go2) {}
+     public void Invoke(Commet go1,Ship go2) {}
      ....
-}
 
-     public void Invoke(GameObject go1,GameObject go2)
+
+     public void Collide(GameObject go1,GameObject go2)
      {
         Handler handler = new Handler();
         if(go1 instanceod(Ship) && go2 instanceof(Statio))
-          handler.Collide((Ship)go1,(Station)go2);
+          handler.Invoke((Ship)go1,(Station)go2);
         ...  
      }
+}
