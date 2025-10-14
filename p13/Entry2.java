a2b5b2beee3b60622070353b47b2dfde4c754156
getmubarak <getmub@gmail.com>
2025-10-13 23:54:28 -0700

    Update Entry2.java

--- p13/Entry2.java
+++ p13/Entry2.java
@@ -1,14 +1,14 @@
 class Handler
 {
-     public void Collide(Ship go1,Station go2) {}
-     public void Collide(Aestroid go1,Station go2) {}
-     public void Collide(Commet go1,Station go2) {}
-     public void Collide(Commet go1,Ship go2) {}
+     public void Invoke(Ship go1,Station go2) {}
+     public void Invoke(Aestroid go1,Station go2) {}
+     public void Invoke(Commet go1,Station go2) {}
+     public void Invoke(Commet go1,Ship go2) {}
      ....
-}
-
-     public void Invoke(GameObject go1,GameObject go2)
+     
+     public void Collide(GameObject go1,GameObject go2)
      {
         Handler handler = new Handler();
-        handler.Collide(go1,go2);
+        handler.Invoke(go1,go2);
      }
+}
