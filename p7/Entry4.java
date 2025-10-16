b4212709d83f18b2cde55d91bd0a6d07e9b56f7d
getmubarak <getmub@gmail.com>
2025-10-15 23:44:47 -0700

    Create Entry4.java

---
+++ p7/Entry4.java
@@ -0,0 +1,46 @@
+class LoanType{
+    boolean isEligible(Customer c);
+}
+class HomeLoan extends LoanType {
+    @Override
+    boolean isEligible(Customer c) {
+        return checkCollateral(c);
+    }
+    boolean checkCollateral(Customer c) { ... }
+}
+
+class PersonalLoan  extends LoanType {
+    @Override
+    boolean isEligible(Customer c) {
+        return checkIncome(c);
+    }
+    boolean checkIncome(Customer c) { ... }
+}
+
+class Eligibility {
+    LoanType loanType;
+    void SetLoanType( LoanType loanType){
+      this.loanType = loanType;
+    }
+    boolean isEligible(Customer c){
+        return checkKYC(c) && checkCreditScore(c) && loanType.isEligible(c);
+    }
+    
+    boolean checkKYC(Customer c) { ... }
+    boolean checkCreditScore(Customer c) { ... }
+}
+
+class EligibilitySalaried extends LoanEligibility {
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkSalarySlips(c);
+    }
+    boolean checkSalarySlips(Customer c) { ... }
+}
+
+class EligibilitySelfEmployed extends LoanEligibility {
+    boolean isEligible(Customer c) {
+        return super.isEligible(c) && checkBusinessDocuments(c);
+    }
+    boolean checkBusinessDocuments(Customer c) { ... }
+}
+
