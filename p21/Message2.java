e2fb240cdc4c69864ecc8fc47ef0bcb0e91baa48
getmubarak <getmub@gmail.com>
2026-05-28 00:03:30 -0700

    Update Message2.java

--- p21/Message2.java
+++ p21/Message2.java
@@ -1,12 +1,12 @@
 
 
 interface Encryption{
-  void encrypt(string data);
-  string decrypt(string cipher);
+  byte[] encrypt(string data);
+  string decrypt(byte[] cipher);
 }
 public class EncryptedMessage {
     private Encryption algorithm;
-    private String text;
+    private byte[] text;
     public EncryptedMessage(Encryption algorithm){
        this.algorithm=algorithm;
    }
@@ -19,21 +19,21 @@ public class EncryptedMessage {
 }
 //*******************************************
 class Aes implements Encryption{
-  void encrypt(string data){
+  byte[] encrypt(string data){
       System.out.println("Encrypting data using AES algorithm");
       /*Code to encrypt data using AES algorithm*/
   }
-  string decrypt(string cipher){
+  string decrypt(byte[] cipher){
         System.out.println("Decrypting cipher using AES algorithm");
         /*Code to decrypt data using AES algorithm*/
   }
 }
 class BlowFish implements Encryption{ 
-  void encrypt(string data){
+  byte[] encrypt(string data){
      System.out.println("Encrypting data using Blowfish algorithm");
      /*Code to encrypt data using Blowfish algorithm */
   }
-  string decrypt(string cipher){
+  string decrypt(byte[] cipher){
       System.out.println("Decrypting cipher using Blowfish algorithm");
       /*Code to decrypt data using Blowfish algorithm */
   }
