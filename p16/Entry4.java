8a6e82aa7a61b2cee89f23e88b0b4caec18fcb31
getmubarak <getmub@gmail.com>
2026-05-28 23:23:25 -0700

    Update Entry4.java

--- p16/Entry4.java
+++ p16/Entry4.java
@@ -3,28 +3,9 @@ class State{
    string name;
    map<string,State> transitions;
 }
-class Definition{
-   string name;
-   State intialState;
-   
-}
-class Instance{
-  Definition sm;
-  State currentState;
-  Instance(Definition sm){
-    this.sm = sm;
-    currentState = sm.intialState;
-  }
-   void fireEvent(string event){
-    if (! currentState.map.contains(event))
-          throw new InvalidStateException();
-
-     currentState = map[event];
-  }
-}
 //*************************************
 
-Definition createStopWatchStateMachine(){
+State createStopWatchStateMachine(){
   State idleState = new State("idle");
   State runningState = new State("running");
   State suspendedState = new State("suspended");
@@ -35,16 +16,18 @@ Definition createStopWatchStateMachine(){
   suspendedState.add("stop", idleState);
   suspendedState.add("resume", runningState);
 
-  return new Definition("StopWatch", idleState);
+  return idleState;
+}
+State fireEvent(State state,string event){
+    if (! state.map.contains(event))
+          throw new InvalidStateException();
+
+     return map[event];
 }
 
 void main(){
-  Definition stopwatch = createStopWatchStateMachine();
-  Instance watch1 =  new Instance(stopwatch);
-  Instance watch2 =  new Instance(stopwatch);
-  
-  watch1.fireEvent("start");
-  watch1.fireEvent("pause");
-  watch1.fireEvent("start");
-  watch2.fireEvent("start");
+  State currentState = createStopWatchStateMachine();
+  currentState= fireEvent(currentState,"start");
+  currentState= fireEvent(currentState,"pause");
+  currentState= fireEvent(currentState,"start");
 }
