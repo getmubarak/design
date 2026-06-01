089a685a211d1d666f0dcb1fc2458ff86ad8d437
getmubarak <getmub@gmail.com>
2026-06-01 00:06:28 -0700

    Update Entry5.java

--- p9/Entry5.java
+++ p9/Entry5.java
@@ -12,21 +12,23 @@ interface Dialog {
 class SADialog : Dialog {}
 class CADialog : Dialog {}
 
+public class DialogFactory{
+	Map<string,Dialog> lookup = new Map<string,Dialog>();
+	public DialogFactory(){
+		lookup.add(SA.getName(),new SADialog());
+		lookup.add(CA.getName(),new CADialog());
+	}
+	public Dialog CreateUI(Account account){
+		return =lookup.get(account.getName()).clone();;
+	}
+}
+
 public class Entry {
-  Map<string,Dialog> lookup = new Map<string,Dialog>();
-  public Entry(){
-    lookup.add(SA.getName(),new SADialog());
-    lookup.add(CA.getName(),new CADialog());
-  }
-	public static void ShowUI(Account account) {
-		Dialog dlg=lookup.get(account.getName()).clone();
-		dlg.Display();			
-	}
-	
-	static void main()
-	{
+	static void main(){
 		Account account = new SA();
-		ShowUI(account);
-		
-	}
+		DialogFactory factory = new DialogFactory();
+		Dialog dlg=factory.createUI(account);
+		if(dlg!=null) {
+			dlg.Display();			
+		}		
 }
