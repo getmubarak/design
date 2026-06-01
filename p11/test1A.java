c55a8fb5b3cb2a806085b8436de6a18e7848ca96
getmubarak <getmub@gmail.com>
2026-05-31 22:01:26 -0700

    Create test1A.java

---
+++ p11/test1A.java
@@ -0,0 +1,35 @@
+//**** contract library (dll/jar) ****************
+interface Broker{
+	void trade();
+}
+//**** notifier library (dll/jar) ****************
+public class StockTradeNotifier {
+	double rate;
+	Broker broker;
+	void SetBroker(Broker broker){
+		this.broker = broker;
+	}
+	public void changeRate(double newRate) {
+		rate = newRate;
+		if(IsGoodTimeTrade()){
+			broker.trade();
+		}
+	}
+	public bool IsGoodTimeTrade(){
+		// domain rules
+		if(rate > ... && rate < ...)
+			....
+	}
+}
+//***** broker library (dll/jar) *******
+class BrokerImp implements Broker{
+   void trade(){ ... logic ... }
+}
+
+//***** client *******
+
+BrokerImp brokerImp = new BrokerImp();
+StockTradeNotifier notifier =  new StockTradeNotifier();
+notifier.SetBroker(brokerImp);
+...
+notifier.changeRate(4545);
